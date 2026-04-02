75873ec40e2fe714bfdb3668c9bcf9c0e738ec28
KacperRutkowski <rutkowski.kacper.karol@gmail.com>
2026-04-02 07:58:11 +0200

    chore: initialize Maven multi-module project (rma-domain, rma-application)

---
+++ rma-application/src/main/java/pl/neopak/rma/RmaApplication.java
@@ -0,0 +1,12 @@
+package pl.neopak.rma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RmaApplication.class, args);
+    }
+}
